964f9ab73354770b963093a8ff7c5cf9d5b7b221
Tobias <toob0102@gmail.com>
2023-11-04 18:12:05 +0100

    Added getbyte method for ease of use when decoding

--- apps/SendTest/message.h
+++ apps/SendTest/message.h
@@ -13,6 +13,14 @@ struct Message{
     msg(msg), nofBytes(nofBytes){
         ESP_LOGI("Message", "Created  message with msg : %llu, nbytes : %d", msg, nofBytes);
     }
+
+    uint8_t getByte(int byteNum){
+        if(byteNum <= nofBytes){
+            return msg >> ((nofBytes-byteNum)*8) & 0xFF;
+        }
+        ESP_LOGE("Message", "Byte index out of range");
+        return 0;
+    }
 };
 }
 #endif //MESSAGE_H
