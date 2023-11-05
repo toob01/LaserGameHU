3ef6d2d898212ed02c9020aa7d3aa9b23bf67e3f
Jorg <jorg.vandenburg@student.hu.nl>
2023-11-05 12:55:21 +0100

    added getters for display

--- apps/allControl/all.hpp
+++ apps/allControl/all.hpp
@@ -15,12 +15,44 @@ namespace crt
         private:
         display& display1;
         speaker& speaker1;
+        int bulletCount = 70;
+        int lives = 99;
+        int timer = 60;
+
+        void bulletShot(){
+            bulletCount--;
+            return;
+        }
+
+        void bulletReload(){
+            bulletCount = 70;
+            vTaskDelay(1000);
+            return;
+        }
+
+        void lifeLost(){
+            lives--;
+            return;
+        }
+
+        void timerDecrease(){
+            timer--;
+            return;
+        }
 
         void main(){
             display1.startUpSet();
-            speaker1.startUpSet();
+            // speaker1.startUpSet();
+            display1.setBulletCount(bulletCount);
+            display1.setLives(lives);
+            display1.setTimer(timer);
+            display1.drawDisplaySet();
+            // speaker1.gunShotSet();
+            // speaker1.hitSet();
+            // speaker1.gunShotSet();
+            // speaker1.gunShotSet();
             display1.gameOverSet();
-            speaker1.gameOverSet();
+            // speaker1.gameOverSet();
 
             for(;;){
                 vTaskDelay(1);
