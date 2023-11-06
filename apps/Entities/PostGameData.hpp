56ec22e22fbf52b9c3db19a0bef0ba6af1ed6cdc
Tobias <toob0102@gmail.com>
2023-11-06 14:09:03 +0100

    entity postgamedata

--- apps/Entities/PostGameData.hpp
+++ apps/Entities/PostGameData.hpp
@@ -1,28 +1,32 @@
 #ifndef POSTGAMEDATA_HPP
 #define POSTGAMEDATA_HPP
 
-#include <array>
-
 struct PostGameData_t {
 private:
     indPostGameData_t GameData[32];
 public:
     PostGameData_t(){}
 
-    void addPlayer(const char* i, int pN, int tN, bool bGO, bool bR){
-        GameData[pN] = indPostGameData_t(i, pN, tN, bGO, bR);
+    void addPlayer(int playerNum, int lives_left, int hits_on_others, int accuracy){
+        GameData[playerNum] = indPostGameData_t(playerNum, lives_left, hits_on_others, accuracy);
+    }
+
+    indPostGameData_t getPlayerStats(int playerNum){
+        for(indPostGameData_t player : GameData){
+            if(playerNum == player.playerNum){
+                return player;
+            }
+        }
     }
 };
 
 struct indPostGameData_t {
-private:
-    int playerNum; 
+    int playerNum;
     int lives_left;
     int hits_on_others;
     int accuracy;      //percentage between 0-100
 
-public:
-    indPostGameData_t(int playerNum, int lives_left, int hits_on_others, int accuracy):
+    indPostGameData_t(int playerNum=0, int lives_left=0, int hits_on_others=0, int accuracy=0):
     playerNum(playerNum), lives_left(lives_left), hits_on_others(hits_on_others), accuracy(accuracy)
     {}
 };
