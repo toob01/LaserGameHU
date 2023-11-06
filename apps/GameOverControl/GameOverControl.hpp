ff439eefddb9581d0987b3ac5f5315149d0277fa
Felix <felix.klaassen@student.hu.nl>
2023-11-06 12:41:00 +0100

    theoretically fixed issue with gameover after time

--- apps/GameOverControl/GameOverControl.hpp
+++ apps/GameOverControl/GameOverControl.hpp
@@ -1,4 +1,9 @@
 #include <CleanRTOS.h>
+#include "GameData.hpp"
+#include "displayControl.hpp"
+#include "speakerControl.hpp"
+#include "shootingControl.hpp"
+#include "recievingHitControl.hpp"
 
 
 
@@ -6,14 +11,16 @@ namespace crt {
     class gameOver : public Task {
         private:
             Flag startFlag;
+            Timer clockTimer;
+            GameData_t& GameData;
 
-            enum GameOverState_t {Idle, EndGame};
-            GameOverState_t GameOverState = GameOverState_t::Idle;
+            enum GameOverState_t {WaitGameOver, WaitTimer};
+            GameOverState_t GameOverState = GameOverState_t::WaitGameOver;
 
 
         public:
-            gameOver( const char *taskName, unsigned int taskPriority, unsigned int taskSizeBytes, unsigned int taskCoreNumber, int timer) :
-                Task( taskName, taskPriority, taskSizeBytes, taskCoreNumber), startFlag(this)
+            gameOver( GameData_t& GameData, const char *taskName, unsigned int taskPriority, unsigned int taskSizeBytes, unsigned int taskCoreNumber, int timer) :
+                Task( taskName, taskPriority, taskSizeBytes, taskCoreNumber), clockTimer(this), startFlag(this), GameData(GameData)
             {
                 start();
             }
@@ -25,20 +32,31 @@ namespace crt {
             void main(){
                 for(;;){
                     switch(GameOverState){
-                        case GameOverState_t::Idle:
+                        case GameOverState_t::WaitGameOver:
+                        //wacht tot start flag is gezet (door gameStateControl)
+                            startFlag.clear();
                             wait(startFlag);
-                            GameOverState = GameOverState_t::EndGame;
-                        break;
-                        case GameOverState_t::EndGame:
+                        //disable hits en shots, geef game over aan met beeld en geluid
                             ShootingControl.disable();
                             ReceivingHitControl.disable();
                             Speaker.gameOverSet();
                             DisplayControl.gameOverSet();
+                        //start SendPostGameDataControl als de game time om is, ga anders naar WaitTimer
+                            if( GameData.getGameTime() == 0){
+                                GameOverState = GameOverState_t::WaitGameOver;
+                                SendPostDataGameDataControl._start();
+                            }else{ GameOverState = GameOverState_t::WaitTimer;}
+                        break;
 
-                            wait(gameTimer);
-                            GameOverState = GameOverState_t::Idle;
-                            SendPostDataGameDataControl._start();
-
+                        case GameOverStat_t::WaitTimer: 
+                        //check om de seconde of gametime voorbij is.
+                            clockTimer.start_periodic(1'000'000); 
+                            wait(clockTimer);
+                            if( GameData.getGameTime() == 0){
+                                clockTimer.stop();
+                                SendPostDataGameDataControl._start();
+                                GameOverState = GameOverState_t::WaitGameOver;
+                            }
                         break;
                     }
                 }
