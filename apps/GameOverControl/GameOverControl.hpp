ee3cc01afb227cd51e9ab7676ce576da550f20d4
Felix <felix.klaassen@student.hu.nl>
2023-11-06 10:52:54 +0100

    GameOverControl main

--- apps/GameOverControl/GameOverControl.hpp
+++ apps/GameOverControl/GameOverControl.hpp
@@ -7,13 +7,42 @@ namespace crt {
         private:
             Flag startFlag;
 
+            enum GameOverState_t {Idle, EndGame};
+            GameOverState_t GameOverState = GameOverState_t::Idle;
+
+
         public:
             gameOver( const char *taskName, unsigned int taskPriority, unsigned int taskSizeBytes, unsigned int taskCoreNumber, int timer) :
                 Task( taskName, taskPriority, taskSizeBytes, taskCoreNumber), startFlag(this)
-            {}
+            {
+                start();
+            }
 
-            void start(){}
-            void main(){}
+            void _start(){
+                startFlag.set();
+            }
+
+            void main(){
+                for(;;){
+                    switch(GameOverState){
+                        case GameOverState_t::Idle:
+                            wait(startFlag);
+                            GameOverState = GameOverState_t::EndGame;
+                        break;
+                        case GameOverState_t::EndGame:
+                            ShootingControl.disable();
+                            ReceivingHitControl.disable();
+                            Speaker.gameOverSet();
+                            DisplayControl.gameOverSet();
+
+                            wait(gameTimer);
+                            GameOverState = GameOverState_t::Idle;
+                            SendPostDataGameDataControl._start();
+
+                        break;
+                    }
+                }
+            }
 
 
 
